84c8df8d3f0151499cc571c28090de1859222299
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2022-05-26 00:25:33 -0400

    added poses for when the player is holding a camera

---
+++ src/main/java/net/hyper_pigeon/polaroidcamera/mixin/BipedEntityModelMixin.java
@@ -0,0 +1,44 @@
+package net.hyper_pigeon.polaroidcamera.mixin;
+
+import net.hyper_pigeon.polaroidcamera.PolaroidCamera;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BipedEntityModel.class)
+public abstract class BipedEntityModelMixin {
+
+    @Shadow
+    public ModelPart head;
+
+    @Shadow
+    public ModelPart rightArm;
+
+    @Shadow
+    public ModelPart leftArm;
+
+    @Inject(at = @At("TAIL"), method = "positionLeftArm")
+    private void cameraLeftArmPoses(LivingEntity livingEntity, CallbackInfo ci){
+        if(livingEntity.getMainHandStack().getItem() == PolaroidCamera.CAMERA_ITEM) {
+            leftArm.roll = 0;
+            leftArm.yaw = 0.15F + this.head.yaw;;
+            leftArm.pitch = -1.5707964F + this.head.pitch;
+        }
+    }
+
+    @Inject(at = @At("TAIL"), method = "positionRightArm")
+    private void cameraRightArmPoses(LivingEntity livingEntity, CallbackInfo ci){
+        if(livingEntity.getMainHandStack().getItem() == PolaroidCamera.CAMERA_ITEM) {
+            rightArm.roll = 0;
+            rightArm.yaw =  -0.1F + this.head.yaw;;
+            rightArm.pitch = -1.5707964F + this.head.pitch;
+        }
+    }
+
+
+}
